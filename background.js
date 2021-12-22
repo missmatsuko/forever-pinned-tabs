9974c7246fdd44fb0210e700446c941ade67c154
Matsuko Friedland <info@matsuko.ca>
2021-12-22 11:32:30 -0800

    Create new google calendar pinned tab on new window

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
+const handleNewWindow = function() {
+  // Create new pinned tab(s)
+  chrome.tabs.create({
+    url: 'https://calendar.google.com', // TODO: use URLs set from options page
+    pinned: true
+  });
+}
 
+chrome.windows.onCreated.addListener(handleNewWindow);
